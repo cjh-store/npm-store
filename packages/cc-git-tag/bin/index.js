02d2dedd8201c50d63a900adaecbdd86ee7755b5
陈俊宏 <1054535289@qq.com>
2021-09-02 16:45:38 +0800

    测试

--- packages/cc-git-tag/bin/index.js
+++ packages/cc-git-tag/bin/index.js
@@ -8,33 +8,37 @@ const GIT_PATH = __dirname;
 const Bump = require("bump-regex");
 const inquirer = require("inquirer");
 
-let newVersion, //新标签
+let newVersion,//新标签
+    oldVersion,
+    promptList, 
   versionHint; //新标签的备注
-const promptList = [
-  {
-    type: "list",
-    message: "请选择升级版本:",
-    name: "type",
-    choices: [
-      {
-        name: "主版本号：重大更新版本,第一次发布正式版选这个",
-        value: "major",
-      },
-      {
-        name: "次版本号：表示功能更新,出现新功能时增加",
-        value: "minor",
-      },
-      {
-        name: "修订号：补丁更新版本,修复bug",
-        value: "patch",
-      },
-      {
-        name: "预发布：预发布版本,即将作为正式版发布",
-        value: "prerelease",
-      },
-    ],
-  },
-];
+
+  promptList = [
+    {
+      type: "list",
+      message: "请选择升级版本:",
+      name: "type",
+      choices: [
+        {
+          name: `主版本号：重大更新版本,第一次发布正式版选这个`,
+          value: "major",
+        },
+        {
+          name: "次版本号：表示功能更新,出现新功能时增加",
+          value: "minor",
+        },
+        {
+          name: "修订号：补丁更新版本,修复bug",
+          value: "patch",
+        },
+        {
+          name: "预发布：预发布版本,即将作为正式版发布",
+          value: "prerelease",
+        },
+      ],
+    },
+  ];
+
 function selectVersion() {
   inquirer
     .prompt(promptList)
@@ -51,40 +55,62 @@ function selectVersion() {
     });
 }
 
-function addTag(type) {
+function selectTag() {
   Git(GIT_PATH)
     .pull()
     .tags(function (err, tags) {
       
-      var oldVersion = tags.latest;
+      oldVersion = tags.latest;
       oldVersion = oldVersion ? oldVersion.slice(1,oldVersion.lastIndexOf('.')): "0.0.0";
-      Bump(
-        {
-          str:
-            "version:" + oldVersion,
-          type,
-        },
-        function (err, out) {
-          //产生新代码
-          newVersion =  `v${out.new}.${dayjs().format('YYMMDD')}`
-         // 产生新标签的备注
-          versionHint =
-            "Relase version " +
-            newVersion +
-            " 发布于 " +
-            dayjs().format("YYYY年MM月DD日  HH:mm:ss");
-
-          Git(GIT_PATH).addAnnotatedTag(newVersion, versionHint, function () {
-            Git(GIT_PATH).pushTags("origin", function () {
-              console.log(
-                "🔖 当前生成tag版本号为:",
-                chalk.white.bgBlue.bold(' '+newVersion+' ') 
-              );
-            });
-          });
-        }
-      );
+      promptList[0].choices.forEach( (e,i)=>{
+        Bump(
+          {
+            str:
+              "version:" + oldVersion ,
+            type:e.value,
+          },
+          function (err, out) {
+            //产生新代码
+           let newTag =  `v${out.new}.${dayjs().format('YYMMDD')}`
+           if(i==0||i==1){
+            e.name=`${e.name.slice(0,4)}(${out.new})${e.name.slice(4)}`
+           }else{
+            e.name=`${e.name.slice(0,3)}(${out.new})${e.name.slice(3)}`
+           }
+          }
+        );
+      })
+      selectVersion()
     });
 }
 
-selectVersion()
+function addTag(type){
+  Bump(
+    {
+      str:
+        "version:" + oldVersion,
+      type,
+    },
+    function (err, out) {
+      //产生新代码
+      newVersion =  `v${out.new}.${dayjs().format('YYMMDD')}`
+     // 产生新标签的备注
+      versionHint =
+        "Release version " +
+        newVersion +
+        " 发布于 " +
+        dayjs().format("YYYY年MM月DD日  HH:mm:ss");
+
+      Git(GIT_PATH).addAnnotatedTag(newVersion, versionHint, function () {
+        Git(GIT_PATH).pushTags("origin", function () {
+          console.log(
+            "🔖 当前生成tag版本号为:",
+            chalk.white.bgBlue.bold(' '+newVersion+' ') 
+          );
+        });
+      });
+    }
+  );
+}
+
+selectTag()
